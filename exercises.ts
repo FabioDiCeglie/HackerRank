8efc0e51b0bfd4aceaeb76b5dd4987d65ae91916
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-04 14:00:28 +0100

    aVeryBigSum

--- exercises.ts
+++ exercises.ts
@@ -1,5 +1,5 @@
 function oddNumbers(l: number, r: number) {
-  const array = [];
+  const array: [] = [];
   for (var i = l; i <= r; i++) {
     array.push(i as never);
   }
@@ -8,3 +8,30 @@ function oddNumbers(l: number, r: number) {
 }
 
 oddNumbers(1, 2);
+
+function compareTriplets(a: Number[], b: Number[]) {
+  let score = [0, 0];
+
+  for (let i = 0; i < a.length; i++)
+    a[i] > b[i] ? score[0]++ : a[i] < b[i] ? score[1]++ : "";
+  return score;
+}
+
+compareTriplets([4, 2], [4, 4]);
+
+function aVeryBigSum(ar: Number[]) {
+  const totalSum = ar.reduce((a, b) => a + b, 0);
+  return totalSum;
+}
+
+// ||
+
+// function aVeryBigSums(ar) {
+//   var total = 0;
+//   for (var i in ar) {
+//     total += ar[i];
+//   }
+//   return total;
+// }
+
+aVeryBigSum([1, 2, 3]);
