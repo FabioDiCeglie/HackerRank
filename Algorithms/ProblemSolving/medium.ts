890fbcb179d3c3c31ec5876e9e985f38adcaedf8
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-06 15:30:00 +0100

    count pages book

--- Algorithms/ProblemSolving/medium.ts
+++ Algorithms/ProblemSolving/medium.ts
@@ -261,3 +261,44 @@ function pageCount(n: number, p: number): number {
 }
 
 pageCount(6, 2);
+
+function pageCountBook(n: number, p: number): number {
+  let numberOfPagesToTurn = [0, 0];
+  let firstPageCounter = 0;
+  let lastPageCounter = 0;
+  if (n % 2 !== 0) {
+    lastPageCounter -= 1;
+  }
+  if (p % 2 !== 0) {
+    firstPageCounter -= 1;
+  }
+  if (p === 1) {
+    return 0;
+  }
+  if (n % 2 !== 0 && p + 1 === n) {
+    return 0;
+  }
+  if (n - 1 === p) {
+    return 1;
+  }
+  for (var i = 0; i < n; i++) {
+    if (i % 2 === 0 && i !== p) {
+      firstPageCounter += 1;
+    }
+    if (i === p) {
+      numberOfPagesToTurn[0] += firstPageCounter;
+    }
+  }
+
+  for (var j = n + 1; j > p - 1; j--) {
+    if (j % 2 === 0 && j !== p) {
+      lastPageCounter += 1;
+    }
+    if (j === p) {
+      numberOfPagesToTurn[1] += lastPageCounter;
+    }
+  }
+  return Math.min(...numberOfPagesToTurn);
+}
+
+pageCountBook(6, 2);
